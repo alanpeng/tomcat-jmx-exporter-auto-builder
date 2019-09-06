c1b56355bb8d2e75c113641aa88d59882e791bfa
Alan Peng <peng.alan@gmail.com>
2019-09-06 15:38:40 +0800

    Update build-tomcat-jmx-exporter.sh

--- build-tomcat-jmx-exporter.sh
+++ build-tomcat-jmx-exporter.sh
@@ -22,9 +22,9 @@ docker rm $TEMP_CONTAINER_NAME
 # Find the line number
 line=`grep -n 'JAVA_OPTS="$JAVA_OPTS $JSSE_OPTS"' DockerBuild-$TIME_STRING/catalina.sh |awk -F ":" '{print $1}'`
 # Add a line
-sed -i "$line r tomcat-jmx-config-string.txt" DockerBuild-$TIME_STRING/catalina.sh
+sed -i -e "$line r tomcat-jmx-config-string.txt" DockerBuild-$TIME_STRING/catalina.sh
 # Replace the port number
-sed -i 's#$JMX_PORT#'"$JMX_AGENT_PORT"'#g' DockerBuild-$TIME_STRING/catalina.sh
+sed -i -e 's#$JMX_PORT#'"$JMX_AGENT_PORT"'#g' DockerBuild-$TIME_STRING/catalina.sh
 
 # Get tomcat-jmx-exporter.yaml and jmx_prometheus_javaagent.jar
 curl -L -o DockerBuild-$TIME_STRING/tomcat-jmx-exporter.yml $TOMCAT_CONFIG_YAML_URL
@@ -32,7 +32,7 @@ curl -L -o DockerBuild-$TIME_STRING/jmx_prometheus_javaagent.jar $JMX_AGENT_URL
 
 # Modify Dockerfile
 cp Dockerfile DockerBuild-$TIME_STRING
-sed -i 's#latest#'"$TOMCAT_VERSION_TAG"'#g' DockerBuild-$TIME_STRING/Dockerfile
+sed -i -e 's#latest#'"$TOMCAT_VERSION_TAG"'#g' DockerBuild-$TIME_STRING/Dockerfile
 
 cd DockerBuild-$TIME_STRING
 docker build -t tomcat-with-jmx-exporter:$TOMCAT_VERSION_TAG .
